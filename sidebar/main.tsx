305c8f5b0ee84df54e1ec459f7f841757ca93fb2
JainamShah28 <shahjainam.com.28@gmail.com>
2025-02-09 23:17:37 +0530

    fix: refactor

--- sidebar/main.tsx
+++ sidebar/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import { ContextsProvider } from './contexts/ContextsContext';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <ContextsProvider>
-            <App />
-        </ContextsProvider>
+        {/* <ContextsProvider> */}
+        {/* <App /> */}
+        <h1>Hello World</h1>
+        {/* </ContextsProvider> */}
     </StrictMode>,
 );
